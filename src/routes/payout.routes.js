958606db4576418a3e7466f5672e14c18a514cc3
azzunique <azunisoftware18@gmail.com>
2025-09-19 11:39:20 +0530

    fix

--- src/routes/payout.routes.js
+++ src/routes/payout.routes.js
@@ -1,13 +1,9 @@
 import express from "express";
-import { initiatePayout, payoutCallback, getPayoutStatus, addBankWithVerifyApi } from "../controllers/payout.controller.js";
+import { addBankWithVerifyApi } from "../controllers/payout.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
-import { bankUpload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
-router.post("/initiate", isAuthenticated, initiatePayout);
-router.post("/callback", payoutCallback);
-router.get("/status/:txnId", isAuthenticated, getPayoutStatus);
-router.post("/verify-add-bank", isAuthenticated, bankUpload.fields([{ name: "passbookImage", maxCount: 1 }]), addBankWithVerifyApi);
+router.post("/verify-add-bank", isAuthenticated, addBankWithVerifyApi);
 
 export default router;
